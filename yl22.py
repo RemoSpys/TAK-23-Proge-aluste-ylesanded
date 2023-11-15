1f1a85900321bef156fe8c27f5b0b7726330fe1c
RemoSpys <104862738+RemoSpys@users.noreply.github.com>
2023-11-15 14:15:28 +0200

    Add files via upload

--- yl22.py
+++ yl22.py
@@ -1,11 +1,29 @@
 import random
 
-options = ["kivi", "paber", "käärid"]
-r = random.choice(options)
+while True:
+    user_action = input("Vali (Kivi, Paber, Käärid): ")
+    possible_actions = ["Kivi", "Paber", "Käärid"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nSa valisid: {user_action}, arvuti valis {computer_action}.\n")
 
-while True
-    a = input("Sisesta kivi, paber või käärid (lõpetamiseks x): ")
+    if user_action == computer_action:
+        print(f"Mõlemad mängijad valisid {user_action}. See on viik!")
+    elif user_action == "Kivi":
+        if computer_action == "Käärid":
+            print("Kivi tapab käärid! Sa võidad!")
+        else:
+            print("Paber tapab kivi! Sa kaotad.")
+    elif user_action == "Paber":
+        if computer_action == "Kivi":
+            print("Paber tapab kivi! Sa võidad!")
+        else:
+            print("Käärid tapab paberi! Sa kaotad.")
+    elif user_action == "Käärid":
+        if computer_action == "Paber":
+            print("Käärid tapab paberi! Sa võidad!")
+        else:
+            print("Kivi tapab käärid! Sa kaotad.")
 
-    if a == "kivi" and r == "käärid" or a == "paber" and r == "kivi" or a == 
-        print("Sinu võit!")
-        elif a == "kivi" and r == "paber" or a == "paber" and r == "käärid" or 
+    play_again = input("Tahad veel mängida? (JAH/EI): ")
+    if play_again.lower() != "jah":
+        break
