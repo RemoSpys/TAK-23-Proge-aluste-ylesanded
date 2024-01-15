169bf1dd2b69af96e622ba557040932b9a4a61b1
RemoSpys <remotammela@gmail.com>
2024-01-15 14:12:37 +0200

    update

--- reddit.py
+++ reddit.py
@@ -21,13 +21,26 @@ commonWords = {'jah', 'aga', 'ei', 'ok', 'kas', 'mina', 'millal', 'kuidas', 'ja'
                 'rohkem', 'nii', 'mina', 'rohkem', 'valja', 'ule', 'mote', 'ma', 'rohkem', 'valja', 'up', 'mooned', 'kuidas', 'ule', 
                 'uhel', 'mis', 'uks', 'mida', 'ei', 'saa', 'ju', 'mu', 'voiks', 'tegi', 'ei', 'tea', 'olid', 'kas', 'see', 'et',
                 'see', 'on', 'See', 'ta', 'meie', 'asi', '', 'siin', 'midagi', 'voib', 'palju', 'need', 'sina', 'Kas', 'nuud',
-                'mis', 'laheb', 'olid', 'me', 'olen', 'ule', 'mida', 'nemad', 'nad', 'kutsuvad', 'sa', 'ei', 'on', 'voib', 'mu', 
+                'mis', 'it', 'a', 'me', 'olen', 'ule', 'mida', 'nemad', 'nad', 'kutsuvad', 'sa', 'ei', 'on', 'voib', 'mu', 
                 'nende', 'nad', 'koos', 'aga', 'umbes', 'sooviksid', 'seal', 'Sina', 'uks', 'tema', 'tema', 'kes', 'tema', 'tema', 
                 'meie', 'asi', '', 'tal', 'oli', 'tema', 'kes', 'tema', 'tema', 'mida', 'teine', 'helesinine', 'siin', 'pole', 
                 'mis', 'oli', 'ma', 'ka', 'siis', 'kui', 'kui', 'motted', 'et', 'see', 'pole', 'kas', 'see', 'on', 'See', 
                 'maletas', 'et', 'ta', 'on', 'Meie', 'veel', 'valja', 'ara', 'on', 'nad', 'utlesid', 'ainult', 'sooviksid', 
                 'teised', 'sisse', 'Ta', 'mida', 'ma', 'et', 'et', 'see', 'mote', 'et', 'nii', 'palju', 'eesti', 'nagu', 'See',
-                'ikka', 'selle', 'kes', 'Ma', 'oma', 'voi', 'vaga', 'tema', 'mingi', 'Kui', 'juba', 'seal', 'koik'}
+                'ikka', 'selle', 'kes', 'Ma', 'oma', 'voi', 'vaga', 'tema', 'mingi', 'Kui', 'juba', 'seal', 'koik'
+                'yes', 'but', 'no', 'ok', 'do', 'work', 'to', 'did', 'Very', 'whether', 'I', 'when', 'how', 'and', 'of', 'the', 'for', 'I', 'this',
+                'is', 'he/she/it', 'they', 'but', 'to be', 'can', 'you', 'that', 'not', 'is', 'you', 'your', 'not', 'could', 'my', 'those', 
+                'to them', 'they', 'together', 'but', 'about', 'how', 'there', 'you', 'and', 'there', 'You', 'from', 'send', 'just',
+                'more', 'so', 'I', 'more', 'out', 'over', 'thought', 'I', 'more', 'out', 'up', 'moons', 'how', 'over', 
+                'one', 'what', 'one', 'what', 'not', 'can', 'know', 'my', 'could', 'made', 'not', 'know', 'were', 'whether', 'this', 'that',
+                'this', 'is', 'This', 'he/she/it', 'our', 'thing', '', 'here', 'something', 'can', 'many', 'these', 'you', 'Are', 'now',
+                'what', 'goes', 'were', 'we', 'am', 'over', 'what', 'they', 'they', 'call', 'you', 'not', 'is', 'can', 'my', 
+                'those', 'they', 'together', 'but', 'about', 'wish', 'there', 'You', 'one', 'he/she/it', 'he/she/it', 'who', 'he/she/it', 'he/she/it', 
+                'our', 'thing', '', 'he/she/it', 'was', 'he/she/it', 'who', 'he/she/it', 'he/she/it', 'what', 'another', 'light blue', 'here', 'is not', 
+                'what', 'was', 'I', 'also', 'then', 'when', 'when', 'thoughts', 'that', 'this', 'is not', 'whether', 'this', 'is', 'This', 
+                'remembered', 'that', 'he/she/it', 'is', 'Our', 'still', 'out', 'off', 'is', 'they', 'said', 'only', 'wish', 
+                'others', 'in', 'He/she', 'what', 'I', 'that', 'that', 'this', 'thought', 'that', 'so', 'much', 'Estonian', 'like', 'This',
+                'still', 'this', 'who', 'I', 'own', 'or', 'very', 'he/she/it', 'some', 'If', 'already', 'there', 'all'}
 
 for submission in subreddit.top(limit=10):
     submission.comments.replace_more(limit=0)
