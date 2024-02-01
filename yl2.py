8ea829c2a38361d920fa70b8383b7e6e1770a9ce
RemoSpys <104862738+RemoSpys@users.noreply.github.com>
2024-02-01 14:32:25 +0200

    Update yl2.py

--- yl2.py
+++ yl2.py
@@ -1,5 +1,8 @@
+# Küsi kasutajalt raadiuse sisendit
 r = float(input("Sisesta raadius  "))
+# Defineeri konstant π
 pi = 3.14
+# Arvuta ringi ümbermõõt ja pindala
 c = 2 * pi * r
 s = r * r * pi
 print("Umbermoot =", c)
