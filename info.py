f685215b604a529e0ff457d8f39093ca504fec9a
RemoSpys <104862738+RemoSpys@users.noreply.github.com>
2024-02-01 14:50:38 +0200

    Create info.py

---
+++ info.py
@@ -0,0 +1,62 @@
+Muutujad
+
+
+Täisarvud (int): Täisarvulised väärtused, näiteks 1, -5, 100.
+
+Ujukomaarvud (float): Ujukomaarvulised väärtused, mis sisaldavad komakohta, näiteks 3.14, -0.5.
+
+Tekst (string): Tekstuaalsed väärtused, näiteks "Tere!" või "123".
+
+Tõeväärtus (boolean): Loogilised väärtused True (tõene) või False (väär).
+
+Nimetamine
+
+
+Muutujad:  "user_age", mitte ua või "age_var"
+
+Funktsioonid: "calculate_total()", mitte "CalcTot" või "ct"
+
+Failid ja kataloogid: "user_data.txt", mitte "udata" või "User Data"
+
+Klassid: "CarModel", mitte "vehicle" või "carmod"
+
+Nimed üldiselt: "calculate_sum", mitte "kalkuleeri_summa" või "calculateSum()"
+
+Andmetüübid
+  
+
+int: Täisarvud, näiteks 5, -12.
+float: Ujukomaarvud, näiteks 3.14, -0.5.
+Tekstiline andmetüüp:
+
+str: Tekstuaalsed väärtused, näiteks "Tere!" või "123".
+Tõeväärtuse andmetüüp:
+
+bool: Loogilised väärtused True või False.
+Kasutamine:
+
+int ja float arvutusteks.
+str teksti hoidmiseks ja töötlemiseks.
+bool loogilistes avaldistes ja tingimuslausetes.
+str ja int funktsioon:
+
+str(42): Teisendab arvu tekstiks, annab tulemuse "42".
+int("42"): Teisendab teksti arvuks, annab tulemuse 42.
+
+Sisend ja väljund
+
+
+Sisend (input):
+
+Küsib kasutajalt sisendit.
+Näide: kasutaja_nimi = input("Sisesta oma nimi: ")
+Väljund (print):
+
+Näitab tulemusi ekraanil.
+Näide: print("Tere, " + kasutaja_nimi + "!")
+Operaator ja string:
+
++ operaator liidab stringe.
+Näide: tervitus = "Tere, " + kasutaja_nimi + "!"
+
+
